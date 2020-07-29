d859d880f6ddbaad360766d2bfe1e9163a976f9d
Mangiang <mangiang@orange.fr>
2020-07-28 22:44:28 -0400

    Fix communication to the backend and to RabbitMQ

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 'use strict'
 
 const express = require('express')
-Object.assign(global, { WebSocket: require('ws') });
-var StompJs = require('@stomp/stompjs');
-const { default: e } = require('express');
-const { json } = require('express');
+Object.assign(global, { WebSocket: require('ws') })
+var StompJs = require('@stomp/stompjs')
+const { default: e } = require('express')
+const { json } = require('express')
 
 // Constants
 const WS_PORT = 8081
@@ -18,65 +18,85 @@ app.get('/', (req, res) => {
 })
 app.listen(HTTP_PORT, HOST)
 console.log(`HTTP Running on http://${HOST}:${HTTP_PORT}`)
-
+const wsArray = []
 
 // Websocket for the frontend
-const wss = new WebSocket.Server({ host: HOST, port: WS_PORT });
+const wss = new WebSocket.Server({ host: HOST, port: WS_PORT })
 console.log(`WS Running on ws://${HOST}:${WS_PORT}`)
-let ws_save = undefined
-wss.on('connection', function connection(ws) {
     // Stomp cliend for RabbitMQ
-    const client = new StompJs.Client({
-        brokerURL: "ws://rabbitmq.default.svc.cluster.local:15674/ws",
-        connectHeaders: {
-            login: "user",
-            passcode: "user"
-        },
-        debug: function(str) {
-            console.log(str);
-        },
-        reconnectDelay: 2000,
-        heartbeatIncoming: 20000,
-        heartbeatOutgoing: 0
-    });
+let client = new StompJs.Client({
+    brokerURL: 'ws://rabbitmq.default.svc.cluster.local:15674/ws',
+    connectHeaders: {
+        login: 'user',
+        passcode: 'user',
+    },
+    debug: function(str) {
+        console.log(str)
+    },
+    reconnectDelay: 2000,
+    heartbeatIncoming: 20000,
+    heartbeatOutgoing: 0,
+})
 
-
-    client.onStompError = function(frame) {
+client.onStompError = function(frame) {
         // Will be invoked in case of error encountered at Broker
         // Bad login/passcode typically will cause an error
         // Complaint brokers will set `message` header with a brief message. Body may contain details.
         // Compliant brokers will terminate the connection after any error
-        console.log('Broker reported error: ' + frame.headers['message']);
-        console.log('Additional details: ' + frame.body);
-    };
-    let subscription = null
-    client.onConnect = function(frame) {
-        // Do something, all subscribes must be done is this callback
-        // This is needed because this will be executed after a (re)connect
-        console.log('Stomp Connected')
-        subscription = client.subscribe('/queue/game_state', function(
-            message,
-        ) {
-            console.log(message)
-            ws.send(JSON.stringify({ type: message.destination, value: message.body }));
-            console.log("message sent to the client")
+        console.log('Broker reported error: ' + frame.headers['message'])
+        console.log('Additional details: ' + frame.body)
+    }
+    // let subscription = null
+client.onConnect = function(frame) {
+    // Do something, all subscribes must be done is this callback
+    // This is needed because this will be executed after a (re)connect
+    console.log('Stomp Connected')
+    const subscription = client.subscribe('/queue/game_state', function(message) {
+        console.log(message)
+        wsArray.forEach((ws) => {
+            ws.send(
+                JSON.stringify({ type: message.destination, value: message.body }),
+            )
         })
-    };
-    client.activate();
+        console.log('message sent to the client')
+    })
+}
+client.activate()
 
+wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
-        const msg = JSON.parse(message);
-        console.log(`received: ${msg}`);
-        console.log(`message type: ${msg.type}`);
-        console.log(`message value: ${msg.value}`);
-        if (msg.type === "input") {
-            client.publish({ destination: "/queue/input", body: JSON.stringify(msg.value) })
-        } else if (msg.type === "connection") {
-            client.publish({ destination: "/queue/connection", body: JSON.stringify("connect") })
+        const msg = JSON.parse(message)
+        console.log(`received: ${msg}`)
+        console.log(`message type: ${msg.type}`)
+        console.log(`message value: ${msg.value}`)
+        if (msg.type === 'input') {
+            client.publish({
+                destination: '/queue/input',
+                body: JSON.stringify(msg.value),
+            })
+        } else if (msg.type === 'connection') {
+            client.publish({ destination: '/queue/connection', body: 'connect' })
         }
-    });
+    })
 
-    ws.on('close', (reasonCode, description) => { subscription.unsubscribe(); });
-
-    // ws.send('something');
-});
+    ws.on('close', (reasonCode, description) => {
+        console.log('Closing connection')
+            // subscription.unsubscribe()
+            // client.deactivate()
+        const index = wsArray.indexOf(ws)
+        if (index > -1) {
+            wsArray.splice(index, 1)
+        }
+    })
+    ws.on('disconnect', (reasonCode, description) => {
+        console.log('Disconnecting')
+            // subscription.unsubscribe()
+            // client.deactivate()
+        const index = wsArray.indexOf(ws)
+        if (index > -1) {
+            wsArray.splice(index, 1)
+        }
+    })
+    wsArray.push(ws)
+        // ws.send('something');
+})
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,19 +5,48 @@ import React, { useRef, useEffect, useState } from 'react'
 import { w3cwebsocket as W3CWebSocket, IMessageEvent } from 'websocket'
 
 const App = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [scene, setScene] = useState<BABYLON.Scene>();
-  const [client, setClient] = useState<W3CWebSocket>();
-  const [objList, setObjList] = useState<BABYLON.Mesh[]>([]);
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const [scene, setScene] = useState<BABYLON.Scene>()
+  const [client, setClient] = useState<W3CWebSocket>()
 
-  useEffect(()=>{
-    let myClient = new W3CWebSocket(
-      'ws://localhost/plugins/cloud-game-engine-game-view-backend/ws',
-    )
-    myClient.onopen = () => {
-      console.log('WebSocket Client Connected')
-      if (canvasRef.current) {
-        myClient.send(JSON.stringify({type: "connection"}));
+  function onmessage(message: IMessageEvent) {
+    console.log(message)
+    const data = JSON.parse(message.data.toString())
+    const value = JSON.parse(data.value.toString())
+    console.log(`value: ${JSON.stringify(value)}`)
+    if (value.type === 'instantiate') {
+      if (value.mesh === 'box') {
+        // {"type": "instantiate", "mesh": "box", "name": "Box", "scale": 4}
+        console.log(`Instancing a ${value.mesh} object`)
+        const box = BABYLON.Mesh.CreateBox(
+          value.name,
+          value.scale ? value.scale : 4.0,
+          scene,
+        )
+        scene!.addMesh(box)
+      }
+    } else if (value.type === 'translation') {
+      // {"type": "translation", "vector": {"x": 0, "y":1, "z":0}, "name": "Box", "distance": 5}
+      console.log(`Apply translation to ${value.name} object`)
+      const mesh:
+        | BABYLON.AbstractMesh
+        | undefined
+        | null = scene!.getMeshByName(value.name)
+      mesh!.translate(
+        new BABYLON.Vector3(value.vector.x, value.vector.y, value.vector.z),
+        value.distance ? value.distance : 0,
+      )
+      console.log(`New position: ${mesh?.position.toString()}`)
+    }
+  }
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      let myClient = new W3CWebSocket(
+        'ws://localhost/plugins/cloud-game-engine-game-view-backend/ws',
+      )
+      myClient.onopen = () => {
+        console.log('WebSocket Client Connected')
         const canvas = canvasRef.current
         const engine = new BABYLON.Engine(canvas, true)
         const sceneTmp = new BABYLON.Scene(engine)
@@ -36,42 +65,28 @@ const App = () => {
             },
             (evt) => {
               console.log(`${evt.sourceEvent.key} button was pressed`)
-              myClient.send(JSON.stringify({type: "input", value: {action: "KEY_DOWN", key: evt.sourceEvent.key}}));
+              myClient.send(
+                JSON.stringify({
+                  type: 'input',
+                  value: { action: 'KEY_DOWN', key: evt.sourceEvent.key },
+                }),
+              )
             },
           ),
         )
-  
         engine.runRenderLoop(() => sceneTmp.render())
-        setScene(sceneTmp);
-
-        myClient.onmessage = (message: IMessageEvent) => {
-          console.log(message)
-          const data = JSON.parse(message.data.toString())
-          const value = JSON.parse(data.value.toString())
-          console.log(`value: ${JSON.stringify(value)}`)
-          if (value.type === "instantiate"){
-            if (value.mesh === "box"){
-              // {"type": "instantiate", "mesh": "box", "name": "Box", "scale": 4}
-              console.log(`Instancing a ${value.mesh} object`);
-              const box = BABYLON.Mesh.CreateBox(value.name, value.scale ? value.scale : 4.0, sceneTmp)
-              setObjList(objList.concat([box]))
-              sceneTmp.addMesh(box);
-            }
-          } else if (value.type === "translation"){
-            // {"type": "translation", "vector": {"x": 0, "y":1, "z":0}, "name": "Box", "distance": 5}
-            console.log(`Apply translation to ${value.name} object`);
-            sceneTmp.meshes.forEach((tmpMesh:BABYLON.AbstractMesh)=> console.log(`Mesh: ${tmpMesh.name}`));
-            const mesh:BABYLON.AbstractMesh|undefined = sceneTmp.meshes.find((tmpMesh:BABYLON.AbstractMesh)=> tmpMesh.name === value.name);
-            mesh!.translate(new BABYLON.Vector3(value.vector.x, value.vector.y, value.vector.z), value.distance ? value.distance : 0);
-            console.log(`New position: ${mesh?.position.toString()}`)
-          }
-        }
-        setClient(myClient);
+        setClient(myClient)
+        setScene(sceneTmp)
       }
     }
+  }, [canvasRef])
 
- 
-  }, []);
+  useEffect(() => {
+    if (client && scene) {
+      client.onmessage = onmessage;
+      client.send(JSON.stringify({ type: 'connection' }))
+    }
+  }, [client, scene])
 
   return <canvas id="babylon-canvas" ref={canvasRef} width={640} height={425} />
 }
